2f58d2f6076532aebd70172d0ec93ea4b1ad417b
Sugarlandcoin <89325634+SugarLandCoin@users.noreply.github.com>
2022-04-20 01:58:42 +0900

    Revert "fix: remaining amount"

--- components/layout/Drawer.tsx
+++ components/layout/Drawer.tsx
@@ -29,7 +29,6 @@ import {BsFillBookmarkPlusFill} from 'react-icons/bs';
 import {BsList} from 'react-icons/bs';
 import {AiOutlineHeart} from 'react-icons/ai';
 import {FiShare} from 'react-icons/fi'
-import {MdCollectionsBookmark} from 'react-icons/md'
 
 interface Props {
   /**
@@ -150,12 +149,6 @@ export default function PermanentDrawerLeft(props: Props) {
                 <ListItemText sx={listItemStyle} primary="Citizen NFTs" />
               </ListItem>
             </Link>
-            <Link href='/mynfts' passHref>
-              <ListItem button key="MyNFTs" selected={router.asPath == '/mynfts'}>
-                <MdCollectionsBookmark/>
-                <ListItemText sx={listItemStyle} primary="My NFTs" />
-              </ListItem>
-            </Link>
           </List>
       </Box>
       <Box sx={boxItemStyle}>
@@ -217,7 +210,6 @@ export default function PermanentDrawerLeft(props: Props) {
                   <Image alt="facebook" src={FACEBOOK} width={'30px'} height={'30px'}/>
                 </a>
               </Link>
-
             </ListItem>
           </List>
       </Box>
--- pages/mint.tsx
+++ pages/mint.tsx
@@ -1,51 +1,11 @@
 import React, { useCallback } from "react";
 import { useContext, useEffect, useState } from 'react';
 import type { NextPage } from 'next';
-import Image from 'next/image';
-import { makeStyles } from "@mui/styles";
 import { styled } from '@mui/material/styles';
 import { useYam } from '../hooks';
 import { Container, Grid, Stack, Typography, Button, Box, Paper, Table, TableBody, TableContainer, TableHead, TableRow } from '@mui/material';
 import TableCell, { tableCellClasses } from '@mui/material/TableCell';
 import { GlobalContext, Web3ModalContext } from '../contexts';
-
-const useStyles = makeStyles(() => ({
-  diamondBox: {
-    margin:'5%',
-    paddingBottom:'5%',
-    // backgroundImage:'url(/nft/tier1.png)',
-    // backgroundImage: `url(${'/nft/tier' + 1 + '.png'})`,
-    backgroundSize:"contain",
-    backgroundRepeat:'no-repeat',
-    backgroundPositionX:'center',
-    backgroundPositionY:'-10px',
-    borderRadius:'10px',
-    // borderStyle:'2px solid white',
-    // boxShadow: '0px 0px 2px 2px #AB6FE9',
-    color:'White',
-
-    
-  },
-  customBadge: {
-    display:'flex',
-    flexDirection:'column',
-    width:'40px',
-    height:'40px',
-    borderRadius:'20px',
-    color: '#AB6FE9',
-    backgroundImage:'url(/images/back/7.jpg)',
-    backgroundSize:'100px 100px',
-    backgroundPosition:'center',
-    // boxShadow: '0px 0px 2px 2px #AB6FE9',
-    boxShadow: '0px 0px 6px 6px lightgray',
-    // borderStyle:'2px solid white',
-    fontSize:'15px',
-    alignItems:'center',
-    paddingTop:"8px",
-    // textAlign:'center',
-  }
-  
-}));
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -57,7 +17,7 @@ const StyledTableCell = styled(TableCell)(({ theme }) => ({
     paddingBottom: '6px',
   },
   [`&.${tableCellClasses.body}`]: {
-    fontSize: 12,
+    fontSize: 10,
     fontWeight: 800,
     color: theme.palette.common.white,
     paddingTop: '6px',
@@ -82,14 +42,12 @@ const StyledTableRow = styled(TableRow)(() => ({
 }));
 
 const Mint: NextPage = () => {
-  const classes = useStyles(); 
   const globalContext = useContext(GlobalContext);
   const { account } = useContext(Web3ModalContext);
   const sugarPrice = globalContext.sugarPrice == null ? 0 : globalContext.sugarPrice;
   const [sellingStatus, setSellingStatus] = useState(true); 
   const [winnerInfo, setWinnerInfo] = useState<any>();
   const [nftPrice, setNftPrice] = useState<number>();
-  const [remainingAmount, setRemainingAmount] = useState<number[]>([]);
   const yamClient = useYam();
 
   const createData = (
@@ -106,12 +64,12 @@ const Mint: NextPage = () => {
   };
 
   const rows = [
-    createData('Tier 1', 'White Diamond', '5,000,000', '30', '5', '1,800', '54,000', '$' + 113),
-    createData('Tier 2', 'Black Diamond', '3,000,000', '30', '4', '1,060', '31,800', '$' + 67),
-    createData('Tier 3', 'Purple Diamond', '2,000,000', '50', '3', '695', '20,850', '$' + 44),
-    createData('Tier 4', 'Rose GOLD Diamond', '1,000,000', '80', '2', '345', '10,350', '$' + 22),
-    createData('Tier 5', 'Pink Diamond', '500,000', '100', '1', '170', '5,100', '$' + 11),
-    createData('Tier 6', 'Gray Diamond', '300,000', '110', '0.5', '100', '3,000', '$' + 6),
+    createData('TIER 1', 'WHITE DIAMOND', '5,000,000', '30', '5', '1,800', '54,000', '$' + (sugarPrice * 54000).toFixed(2)),
+    createData('TIER 2', 'BLACK DIAMOND', '3,000,000', '30', '4', '1,000', '30,000', '$' + (sugarPrice * 30000).toFixed(2)),
+    createData('TIER 3', 'PURPLE DIAMOND', '2,000,000', '50', '3', '700', '21,000', '$' + (sugarPrice * 21000).toFixed(2)),
+    createData('TIER 4', 'ROSE GOLD DIAMOND', '1,000,000', '80', '2', '350', '10,500', '$' + (sugarPrice * 10500).toFixed(2)),
+    createData('TIER 5', 'PINK DIAMOND', '500,000', '100', '1', '180', '5,400', '$' + (sugarPrice * 5400).toFixed(2)),
+    createData('TIER 6', 'GREY DIAMOND', '300,000', '110', '0.5', '100', '3,000', '$' + (sugarPrice * 3000).toFixed(2)),
   ];
 
   
@@ -121,14 +79,6 @@ const Mint: NextPage = () => {
         if(yamClient != undefined) {
           const sellingStatusRes = await yamClient.contracts.contractsMap['SugarNFT'].methods.getSellingStatus().call(); //Selling
           const winnerInfoRes = await yamClient.contracts.contractsMap['SugarNFT'].methods.getWinnerInfo(account).call(); 
-          const remains: number[] = new Array(6);
-          for(let i=1; i<=6; i++){
-            const temp = await yamClient.contracts.contractsMap['SugarNFT'].methods.getRemainingAmount(i).call();
-            remains[i] = temp;
-          }
-          // console.log("------------------------" + remains);
-          setRemainingAmount(remains);
-          console.log(remainingAmount);
           setSellingStatus(sellingStatusRes);
           setWinnerInfo(winnerInfoRes);
         }
@@ -216,23 +166,16 @@ const Mint: NextPage = () => {
         {
           (new Array(6).fill(0)).map((_, index) => {
             return (
-
-              <Grid key={index} item xs={12} sm={6} md={4} lg={2} >
-                <Stack direction="column" justifyContent='center' alignItems='center' className={classes.diamondBox}
-                style={{backgroundImage:`url(${'/nft/tier'+(index+1)+'.png'})`}}
-                >
-                <Typography variant="subtitle1" className={classes.customBadge}>{remainingAmount[index+1]}</Typography>
-                
-                  <Box 
-                    // component="img"
-                    // src={`/nft/tier (${index + 1}).png`}
+              <Grid key={index} item xs={12} sm={6} md={4} lg={2}>
+                <Stack direction="column" justifyContent='center' alignItems='center'>
+                  <Box component="img"
+                    src={`/nft/tier (${index + 1}).png`}
                     sx={{
-                      height: 300,
-                      width: 180,
+                      height: 240,
+                      width: 150,
                       borderRadius: 3,
                     }} 
-                  ></Box>
-                     
+                  />
                   {sellingStatus ? (
                     <Button sx={{width: 150}}
                     onClick = {() => handleMint(index + 1)}
@@ -250,7 +193,7 @@ const Mint: NextPage = () => {
             )
           })
         }
-        <Grid item xs={12} md={12} mt={5} >
+        <Grid item xs={12} md={12} mt={5}>
           <Box >
             <Grid container spacing={2}>
               <Grid item xs={12} sm={12} md={9}>
@@ -278,301 +221,3 @@ const Mint: NextPage = () => {
 };
 
 export default Mint;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useCallback } from "react";
-// import { useContext, useEffect, useState } from 'react';
-// import type { NextPage } from 'next';
-// import Image from 'next/image';
-// import { makeStyles } from "@mui/styles";
-// import { styled } from '@mui/material/styles';
-// import Badge from '@mui/material/Badge';
-// import { useYam } from '../hooks';
-// import { Container, Grid, Stack, Typography, Button, Box, Paper, Table, TableBody, TableContainer, TableHead, TableRow } from '@mui/material';
-// import TableCell, { tableCellClasses } from '@mui/material/TableCell';
-// import { GlobalContext, Web3ModalContext } from '../contexts';
-
-// const useStyles = makeStyles(() => ({
-//   diamondBox: {
-//     margin:'5%',
-//     paddingBottom:'5%',
-//     backgroundSize:"100% 100%",
-//     borderRadius:'10px',
-//     color:'White',
-//     boxShadow: '0px 0px 2px 2px #AB6FE9',
-//     backgroundColor: 'rgba(38, 14, 238, 0.25)',
-    
-//   },
-//   remainDiamonds: {
-//     display:'flex',
-//   }
-  
-// }));
-
-// const StyledTableCell = styled(TableCell)(({ theme }) => ({
-//   [`&.${tableCellClasses.head}`]: {
-//     backgroundColor: '#513B7C',
-//     color: theme.palette.common.white,
-//     fontSize: 12,
-//     fontWeight: 800,
-//     paddingTop: '6px',
-//     paddingBottom: '6px',
-//   },
-//   [`&.${tableCellClasses.body}`]: {
-//     fontSize: 12,
-//     fontWeight: 800,
-//     color: theme.palette.common.white,
-//     paddingTop: '6px',
-//     paddingBottom: '6px',
-//   },
-// }));
-
-// const StyledTableRow = styled(TableRow)(() => ({
-//   '&:nth-of-type(even)': {
-//     backgroundColor: 'transparent',
-//     // backgroundColor: '#70569D',
-//     borderRadius: 10,
-//   },
-//   '&:nth-of-type(odd)': {
-//     backgroundColor: 'transparent',
-//     borderRadius: 10,
-//   },
-//   // hide last border
-//   '&:last-child td, &:last-child th': {
-//     border: 0,
-//   },
-// }));
-
-// const Mint: NextPage = () => {
-//   const classes = useStyles(); 
-//   const globalContext = useContext(GlobalContext);
-//   const { account } = useContext(Web3ModalContext);
-//   const sugarPrice = globalContext.sugarPrice == null ? 0 : globalContext.sugarPrice;
-//   const [sellingStatus, setSellingStatus] = useState(true); 
-//   const [winnerInfo, setWinnerInfo] = useState<any>();
-//   const [nftPrice, setNftPrice] = useState<number>();
-//   const [remainingAmount, setRemainingAmount] = useState<number[]>([]);
-//   const yamClient = useYam();
-
-//   const createData = (
-//     tier: string,
-//     whale: string,
-//     balance: string,
-//     quantity: string,
-//     cost: string,
-//     day: string,
-//     month: string,
-//     worth: string,
-//   ) => {
-//     return { tier, whale, balance, quantity, cost, day, month, worth };
-//   };
-
-//   const rows = [
-//     createData('Tier 1', 'White Diamond', '5,000,000', '30', '5', '1,800', '54,000', '$' + 113),
-//     createData('Tier 2', 'Black Diamond', '3,000,000', '30', '4', '1,060', '31,800', '$' + 67),
-//     createData('Tier 3', 'Purple Diamond', '2,000,000', '50', '3', '695', '20,850', '$' + 44),
-//     createData('Tier 4', 'Rose GOLD Diamond', '1,000,000', '80', '2', '345', '10,350', '$' + 22),
-//     createData('Tier 5', 'Pink Diamond', '500,000', '100', '1', '170', '5,100', '$' + 11),
-//     createData('Tier 6', 'Gray Diamond', '300,000', '110', '0.5', '100', '3,000', '$' + 6),
-//   ];
-
-  
-//   useEffect(() => {
-//     const getSellingStatus = async () => {
-//       try {
-//         if(yamClient != undefined) {
-//           const sellingStatusRes = await yamClient.contracts.contractsMap['SugarNFT'].methods.getSellingStatus().call(); //Selling
-//           const winnerInfoRes = await yamClient.contracts.contractsMap['SugarNFT'].methods.getWinnerInfo(account).call(); 
-//           const remains: number[] = new Array(6);
-//           for(let i=1; i<=6; i++){
-//             const temp = await yamClient.contracts.contractsMap['SugarNFT'].methods.getRemainingAmount(i).call();
-//             remains[i] = temp;
-//           }
-//           // console.log("------------------------" + remains);
-//           setRemainingAmount(remains);
-//           console.log(remainingAmount);
-//           setSellingStatus(sellingStatusRes);
-//           setWinnerInfo(winnerInfoRes);
-//         }
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     };
-//     getSellingStatus();
-//   }, [yamClient]);
-
-//   const handleMint = async (id: number) => {
-//     if(yamClient != undefined) {
-//       const res = await yamClient.contracts.contractsMap['SugarNFT'].methods.getPricePerNFT(id).call();
-//       setNftPrice(res);
-//       await yamClient.contracts.contractsMap['SugarNFT'].methods.mint(id).send({from:account, value:nftPrice});
-//     }
-//   };
-
-//   const handleClaim = async () => {
-//     if(yamClient != undefined) {
-//       await yamClient.contracts.contractsMap['SugarNFT'].methods.airdrop().send({from: account});
-//     }
-//   };
-
-//   const getEnableStatus = useCallback((id: number): boolean => {
-//     // Mint phase
-//     if (sellingStatus) {
-//       return true;
-//     }
-//     // Airdrop phase
-//     if (winnerInfo[1]) {
-//       return false;
-//     }
-//     return (id == winnerInfo[0]);
-//   }, [sellingStatus, winnerInfo]);
-  
-//   const _renderTable = (_rows: any) => {
-//     return (
-//       <TableContainer component={Paper} sx={{backgroundColor: 'transparent', color: 'white'}} elevation={0}>
-//         <Table sx={{ minWidth: 100 }} aria-label="simple table">
-//           <TableHead>
-//             <TableRow sx={{borderRadius: 30}}>
-//               <StyledTableCell>TIERS</StyledTableCell>
-//               <StyledTableCell align="right">WHALE CARD TYPE</StyledTableCell>
-//               <StyledTableCell align="right">MINIMUM BALANCE</StyledTableCell>
-//               <StyledTableCell align="right">TOTAL QUANTITY</StyledTableCell>
-//               <StyledTableCell align="right">MINT COST IN BNB</StyledTableCell>
-//               <StyledTableCell align="right">REWARDS /NFT/DAY</StyledTableCell>
-//               <StyledTableCell align="right">REWARDS /NFT/MONTH</StyledTableCell>
-//               <StyledTableCell align="right">REWARDS WORTH</StyledTableCell>
-//             </TableRow>
-//           </TableHead>
-//           <TableBody>
-//             {_rows.map((row: any) => (
-//               <StyledTableRow 
-//                 key={row.tier}
-//                 sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-//               >
-//                 <StyledTableCell component="th" scope="row">
-//                   {row.tier}
-//                 </StyledTableCell>
-//                 <StyledTableCell align="right">{row.whale}</StyledTableCell>
-//                 <StyledTableCell align="right">{row.balance}</StyledTableCell>
-//                 <StyledTableCell align="right">{row.quantity}</StyledTableCell>
-//                 <StyledTableCell align="right">{row.cost}</StyledTableCell>
-//                 <StyledTableCell align="right">{row.day}</StyledTableCell>
-//                 <StyledTableCell align="right">{row.month}</StyledTableCell>
-//                 <StyledTableCell align="right">{row.worth}</StyledTableCell>
-//               </StyledTableRow >
-//             ))}
-//           </TableBody>
-//         </Table>
-//       </TableContainer>
-//     );
-//   }
-
-//   return (
-//     <Container maxWidth="xl" sx={{mt: 3, p: 10}}>
-//       {/* This is Minting Page for NFT */}
-//       <Grid container>
-//         <Grid item xs={12} sm={12} md={12} p={2} mb={3}>
-//           <Typography variant='h4'>COLLECT DIAMOND NFTS & BOOST YOUR $SUGAR REWARDS</Typography>
-//           <Typography variant='h6'>Monthly Rewards Pool = 5,280,000 SUGAR [Worth ${(sugarPrice * 5280000).toFixed(2)}*]</Typography>
-//         </Grid>
-//         {
-//           (new Array(6).fill(0)).map((_, index) => {
-//             return (
-
-//               <Grid key={index} item xs={12} sm={6} md={4} lg={2}>
-
-//                 <Stack direction="column" justifyContent='center' alignItems='center' className={classes.diamondBox}>
-//                 <Box className={classes.remainDiamonds}>
-//                 <Badge 
-//                     anchorOrigin={{
-//                       vertical: 'top',
-//                       horizontal: 'right',
-//                     }}
-//                     badgeContent={remainingAmount[index+1] + 'NFTs'} color="primary">
-                    
-//                     <Image src={'/images/util/diamonds2.png'} height={'30px'} width='80px' />  
-//                 </Badge>             
-                  
-//                 </Box>
-                
-//                   <Box component="img"
-//                     src={`/nft/tier (${index + 1}).png`}
-//                     sx={{
-//                       height: 300,
-//                       width: 180,
-//                       borderRadius: 3,
-//                     }} 
-//                   />
-                     
-//                   {sellingStatus ? (
-//                     <Button sx={{width: 150}}
-//                     onClick = {() => handleMint(index + 1)}
-//                     >Mint Now
-//                     </Button>
-//                   ) : (
-//                     <Button sx={{width: 150}}
-//                       onClick = {() => handleClaim()}
-//                       disabled = {!getEnableStatus(index + 1)}
-//                     >Claim NFT
-//                     </Button>
-//                   )}
-//                 </Stack>
-//               </Grid>
-//             )
-//           })
-//         }
-//         <Grid item xs={12} md={12} mt={5} >
-//           <Box >
-//             <Grid container spacing={2}>
-//               <Grid item xs={12} sm={12} md={9}>
-//                 <Box>
-//                   { _renderTable(rows) }
-//                 </Box>
-//               </Grid>
-//               <Grid item xs={12} sm={12} md={3}>
-//                 <Stack direction="column">
-//                   <Typography variant="subtitle1">MINT CONDITION</Typography>
-//                   <Typography variant="label1">Whitelisted Winners can mint for free but should have minimum SUGAR BALANCE for their Tier.</Typography>
-//                   <Typography variant="subtitle1">REWARDS CONDITION</Typography>
-//                   <Typography variant="label1">Daily rewards will reflect only if you have minimum SUGAR balance for your NFT tier.</Typography>
-//                   <Typography variant="subtitle1">MULTIPLE NFTS</Typography>
-//                   <Typography variant="label1">For Multiple NFTs, Wallet must have Combined minimum balance for all NFTs owned.</Typography>
-//                   <Typography variant="label1">Each NFT Trade has built in 10% Royalties back to Sugarland&apos;ags Royalty Wallet.</Typography>
-//                 </Stack>
-//               </Grid>
-//             </Grid>
-//           </Box>
-//         </Grid>
-//       </Grid>
-//     </Container>
-//   );
-// };
-
-// export default Mint;
-
